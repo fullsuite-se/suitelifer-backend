67e6463c13a4d3306c2f1ab9e3a3be8d335b0124
domingohernani <dev.hernani.domingo@gmail.com>
2025-02-14 16:32:47 +0800

    bug fixed

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 import express from "express";
 import exampleRoutes from "./routes/exampleRoutes.js";
 
+import cors from "cors";
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
 const app = express();
 
 app.use(express.json());
