a8959918003f893ec269e6bf4782e11d97e678e4
jausalcedo <jau.salcedo04@gmail.com>
2025-03-04 00:49:32 +0800

    feat: user model and controller template for knex

--- config/db.js
+++ config/db.js
@@ -1,12 +1,32 @@
 import mysql from "mysql2";
+import knex from "knex";
+
+import dotenv from "dotenv";
+dotenv.config();
+
 const DB_HOST = process.env.DB_HOST;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_DATABASE = process.env.DB_DATABASE;
 
-mysql.createPool({
+export const pool = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_DATABASE,
+});
+
+console.log(process.env.PORT);
+
+export const db = knex({
+  client: "mysql2",
+  connection: {
+    host: DB_HOST,
+    port: 3306,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    connectTimeout: 60000,
+  },
+  pool: { min: 2, max: 10 },
 });
