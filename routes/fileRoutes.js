e2631202b4f76ff4cd5c73a20333469fd45fdb6f
domingohernani <dev.hernani.domingo@gmail.com>
2025-03-05 15:43:24 +0800

    feat: implement image upload controller and update file upload route

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -1,26 +1,9 @@
 import express from "express";
 import upload from "../utils/multer.js";
-import cloudinary from "../utils/cloudinary.js";
+import { uploadImage } from "../controllers/fileController.js";
 
 const router = express.Router();
 
-router.post("/upload", upload.single("image"), async (req, res) => {
-  try {
-    if (!req.file) {
-      return res.status(400).json({ error: "No file uploaded" });
-    }
-    const result = await new Promise((resolve, reject) => {
-      const stream = cloudinary.uploader.upload_stream(
-        { folder: "suitelifer/blogs" },
-        (error, result) => (error ? reject(error) : resolve(result))
-      );
-      stream.end(req.file.buffer);
-    });
-
-    res.json({ imageUrl: result.secure_url });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+router.post("/upload-image", upload.single("image"), uploadImage);
 
 export default router;
