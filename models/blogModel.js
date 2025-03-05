0ca983adfb7a611774eda4db0f39415450cd85b5
domingohernani <dev.hernani.domingo@gmail.com>
2025-03-05 17:02:15 +0800

    feat: implement addBlog function in blog model

--- models/blogModel.js
+++ models/blogModel.js
@@ -1,7 +1,31 @@
 import { db } from "../config/db.js";
+import { v7 as uuidv7 } from "uuid";
+
+const table = "sl_employee_blogs";
 
 export const Blogs = {
   getAllBlogs: async () => {
-    return await db.select("*").from("sl_employee_blogs");
+    return await db.select("*").from(table);
+  },
+
+  addBlog: async (data) => {
+    const blog = {
+      eblog_id: uuidv7(),
+      title: data.title,
+      description: data.description,
+      created_by: userId,
+      updated_by: userId,
+    };
+
+    await knex(table).insert(blog);
+
+    const imagesData = data.images.map((image) => {
+      return {
+        content_id: data.eblog_id,
+        image_url: image.url,
+      };
+    });
+
+    return blog;
   },
 };
