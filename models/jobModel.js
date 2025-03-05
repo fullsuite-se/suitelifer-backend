ed49d17f457708f24fa7816bd27d6a1c76971408
nodejaus <jau.salcedo04@gmail.com>
2025-03-05 14:06:57 +0800

    feat: complete job model controller route

--- models/jobModel.js
+++ models/jobModel.js
@@ -1,10 +1,86 @@
+import { company_id } from "../config/companyConfig.js";
 import { db } from "../config/db.js";
+import { v7 as uuidv7 } from "uuid";
+
+const table = db("sl_company_jobs");
 
 export const Job = {
   getAllJobs: async () => {
-    return await db.select("*").from("sl_company_jobs").join('job_setup', { 'job_setup.setup_id': 'sl_company_jobs.setup_id'});
+    return await db
+      .select("*")
+      .from("sl_company_jobs")
+      .join("job_setup", { "job_setup.setup_id": "sl_company_jobs.setup_id" });
+  },
+  getOpenJobs: async () => {
+    return db
+      .select("*")
+      .from("sl_company_jobs")
+      .join("job_setup", { "job_setup.setup_id": "sl_company_jobs.setup_id" })
+      .where({ is_open: true });
   },
   getJobDetails: async (job_id) => {
-    return await db("sl_company_jobs").where("job_id", job_id);
+    return await db
+      .select("*")
+      .from("sl_company_jobs")
+      .join("job_setup", { "sl_company_jobs.setup_id": "job_setup.setup_id" })
+      .join("sl_job_details", {
+        "sl_company_jobs.job_id": "sl_job_details.job_id",
+      })
+      .join("sl_salary_ranges", {
+        "sl_job_details.salary_range_id": "sl_salary_ranges.salary_range_id",
+      })
+      .join("sl_job_industries", {
+        "sl_company_jobs.industry_id": "sl_job_industries.job_ind_id",
+      });
+  },
+  insertJob: async (
+    title,
+    description,
+    employment_type,
+    setup_id,
+    is_open,
+    industry_id,
+    user_id
+  ) => {
+    await db.insert(
+      uuidv7(),
+      company_id,
+      title,
+      description,
+      employment_type,
+      setup_id,
+      is_open,
+      industry_id,
+      Date.now(),
+      user_id,
+      null,
+      null
+    );
+  },
+  updateJob: async (
+    job_id,
+    title,
+    description,
+    employment_type,
+    setup_id,
+    is_open,
+    industry_id,
+    user_id
+  ) => {
+    await table.where({ job_id: job_id }).update(
+      {
+        title: title,
+        description: description,
+        employment_type: employment_type,
+        setup_id: setup_id,
+        is_open: is_open,
+        industry_id: industry_id,
+        user_id: user_id,
+      },
+      ["job_id", "title"]
+    );
+  },
+  deleteJob: async (job_id) => {
+    await table.where("job_id", job_id).del();
   },
 };
--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,8 +1,14 @@
 import express from 'express';
-import { getJobs } from '../controllers/jobController.js';
+import { getJobs, getOpenJobs, insertJob, updateJob } from '../controllers/jobController.js';
 
 const router = express.Router();
 
 router.get("/all-jobs", getJobs);
+
+router.get("/all-open-jobs", getOpenJobs);
+
+router.post("/add-job", insertJob);
+
+router.post("/edit-job", updateJob);
 
 export default router;
