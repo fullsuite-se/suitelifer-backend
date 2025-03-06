4b6d9a8f2c405772420dd07bd7a88893b67e0efe
domingohernani <dev.hernani.domingo@gmail.com>
2025-03-06 15:33:20 +0800

    feat: add uploadImages function to handle multiple image uploads with dynamic folder structure

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,4 +1,5 @@
 import cloudinary from "../utils/cloudinary.js";
+import { Image } from "../models/imageModel.js";
 
 export const uploadImage = async (req, res) => {
   try {
@@ -14,6 +15,45 @@ export const uploadImage = async (req, res) => {
     });
 
     res.json({ imageUrl: result.secure_url });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const uploadImages = async (req, res) => {
+  const { folder, id } = req.params;
+
+  try {
+    if (!req.files || req.files.length === 0) {
+      return res.status(400).json({ error: "No files uploaded" });
+    }
+
+    const uploadPromises = req.files.map((file) => {
+      return new Promise((resolve, reject) => {
+        const stream = cloudinary.uploader.upload_stream(
+          { folder: `suitelifer/${folder}/${id}` },
+          (error, result) =>
+            error ? reject(error) : resolve(result.secure_url)
+        );
+        stream.end(file.buffer);
+      });
+    });
+
+    const imageUrls = await Promise.all(uploadPromises);
+
+    try {
+      await Image.addImages(id, imageUrls);
+    } catch (dbError) {
+      console.error("Database error:", dbError);
+      return res
+        .status(500)
+        .json({ error: "Failed to save images in the database" });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Upload successful!",
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
