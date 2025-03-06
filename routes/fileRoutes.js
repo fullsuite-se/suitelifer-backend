2f4b6c16a4e8db3451e915da6ce6ee2cc6c53258
domingohernani <dev.hernani.domingo@gmail.com>
2025-03-06 15:32:59 +0800

    feat: update image upload route to support multiple images and dynamic folder structure

--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import upload from "../utils/multer.js";
-import { uploadImage } from "../controllers/fileController.js";
+import { uploadImages } from "../controllers/fileController.js";
 
 const router = express.Router();
 
-router.post("/upload-image", upload.single("image"), uploadImage);
+router.post(
+  "/upload-image/:folder/:id",
+  upload.array("images", 10),
+  uploadImages
+);
 
 export default router;
