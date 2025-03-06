e2537449aef12e3e4f2f32b7c918692a839143f5
domingohernani <dev.hernani.domingo@gmail.com>
2025-03-06 09:59:13 +0800

    feat: add route for adding employee blog with JWT verification and image upload

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,8 +1,17 @@
 import express from "express";
-import { getAllBlogs } from "../controllers/blogController.js";
+import { addBlog, getAllBlogs } from "../controllers/blogController.js";
+import upload from "../utils/multer.js";
+import verifyJWT from "../middlewares/verifyJWT.js";
 
 const router = express.Router();
 
 router.get("/all-employee-blog", getAllBlogs);
+
+router.post(
+  "/add-employee-blog",
+  verifyJWT,
+  upload.array("images", 10),
+  addBlog
+);
 
 export default router;
