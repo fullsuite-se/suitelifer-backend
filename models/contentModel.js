1344aeed0587af28e9a06e147a8328f28d5b987f
nodejaus <jau.salcedo04@gmail.com>
2025-03-06 13:12:25 +0800

    feat: use alias when sending back info to frontend

--- models/contentModel.js
+++ models/contentModel.js
@@ -2,6 +2,24 @@ import { db } from "../config/db.js";
 
 export const Content = {
   getAboutUs: async () => {
-    return await db("sl_content").orderBy("content_id", "desc").first();
+    return await db
+      .select(
+        "hero_video AS heroVideo",
+        "text_banner AS textBanner",
+        "hero_image AS heroImage",
+        "story_img AS storyImg",
+        "mission_slogan AS missionSlogan",
+        "mission",
+        "vision_slogan AS visionSlogan",
+        "vision",
+        "day_in_pod_url AS dayInPodUrl",
+        "created_at AS createdAt",
+        db.raw(
+          "CONCAT(hris_user_infos.first_name, ' ', LEFT(hris_user_infos.middle_name, 1), '. ', hris_user_infos.last_name) AS createdBy"
+        )
+      )
+      .from("sl_content")
+      .orderBy("content_id", "desc")
+      .first();
   },
 };
