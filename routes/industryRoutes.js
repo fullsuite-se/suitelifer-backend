ddb3bcef0763072885bf1522f1a7ad348eade1f1
nodejaus <jau.salcedo04@gmail.com>
2025-03-10 09:51:03 +0800

    feat: implement industry management with CRUD operations and routing

---
+++ routes/industryRoutes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  deleteIndustry,
+  getAllIndustries,
+  getAllIndustriesHR,
+  getAllIndustriesPR,
+  insertIndustry,
+  updateIndustry,
+} from "../controllers/industryController";
+
+const router = express.Router();
+
+router.get("/get-all-industries", getAllIndustries);
+
+router.get("/get-all-industries-hr", getAllIndustriesHR);
+
+router.get("/get-all-industries-pr", getAllIndustriesPR);
+
+router.post("/add-industry", insertIndustry);
+
+router.post("/edit-industry", updateIndustry);
+
+router.post("/delete-industry", deleteIndustry);
+
+export default router;
