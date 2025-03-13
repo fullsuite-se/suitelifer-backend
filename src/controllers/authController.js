f3e85e54c7d9ccbcb40fe2806bbb20847d641571
domingohernani <dev.hernani.domingo@gmail.com>
2025-03-13 10:33:36 +0800

    feat: secure cookies by setting 'secure' flag to true for authentication tokens

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -40,13 +40,13 @@ export const login = async (req, res) => {
 
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
-    secure: false, // Change this to true if using HTTPS
+    secure: true, // Change this to true if using HTTPS
     sameSite: "Strict",
   });
 
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
-    secure: false, // Change this to true if using HTTPS
+    secure: true, // Change this to true if using HTTPS
     sameSite: "Strict",
   });
 
@@ -55,7 +55,7 @@ export const login = async (req, res) => {
 export const logout = (req, res) => {
   res.cookie("accessToken", "", {
     httpOnly: true,
-    secure: false, // Change this to true if using HTTPS
+    secure: true, // Change this to true if using HTTPS
     sameSite: "Strict",
     path: "/",
     expires: new Date(0), // Expire immediately
@@ -63,7 +63,7 @@ export const logout = (req, res) => {
 
   res.cookie("refreshToken", "", {
     httpOnly: true,
-    secure: false, // Change this to true if using HTTPS
+    secure: true, // Change this to true if using HTTPS
     sameSite: "Strict",
     path: "/",
     expires: new Date(0),
@@ -107,7 +107,7 @@ export const refreshToken = async (req, res) => {
 
       res.cookie("accessToken", newAccessToken, {
         httpOnly: true,
-        secure: false, // Change this to true if using HTTPS
+        secure: true, // Change this to true if using HTTPS
         sameSite: "Strict",
       });
 
@@ -140,55 +140,3 @@ export const getServices = async (req, res) => {
       .json({ message: "Server error", error: error.message });
   }
 };
-
-// EXPERIMENTAL API
-// export const getSpotifyToken = async code => {
-//   // CODE VERIFIER
-//   const generateRandomString = (length) => {
-//     const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-//     const values = crypto.getRandomValues(new Uint8Array(length));
-//     return values.reduce((acc, x) => acc + possible[x % possible.length], "");
-//   }
-
-//   const codeVerifier = generateRandomString(64);
-
-//   // CODE CHALLENGE
-//   const sha256 = async (plain) => {
-//     const encoder = new TextEncoder();
-//     const data = encoder.encode(plain);
-//     return window.crypto.subtle.digest("SHA-256", data);
-//   }
-
-//   const base64encode = (input) => {
-//     return btoa(String.fromCharCode(...new Uint8Array(input)))
-//     .replace(/=/g, '')
-//     .replace(/\+/g, '-')
-//     .replace(/\//g, '_');
-//   }
-
-//   const hashed = await sha256(codeVerifier);
-//   const codeChallenge = base64encode(hashed);
-
-//   const clientId = process.env.SPOTIFY_CLIENT_ID;
-//   const redirectUri = "http://localhost:5173";
-
-//   const scope = "user-read-private user-read-email";
-//   const authUrl = new URL("https://accounts.spotify.com/authorize");
-
-//   window.localStorage.setItem('code_verifier', codeVerifier);
-
-//   const params = {
-//     response_type: "code",
-//     client_id: clientId,
-//     scope,
-//     code_challenge_method: "256",
-//     code_challenge: codeChallenge,
-//     redirectUri: redirectUri,
-//   }
-
-//   authUrl.search = new URLSearchParams(params).toString();
-//   window.location.href = authUrl.toString();
-
-//   const urlParams = new URLSearchParams(window.location.search);
-//   var code = urlParams.get('code');
-// }
