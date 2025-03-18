73a68dcc965f5df37026d6195f7f3091ff7c6ffb
domingohernani <dev.hernani.domingo@gmail.com>
2025-03-18 18:17:55 +0800

    feat: add getAllCompanyBlogs method and update employee blog methods in blog model

--- src/models/blogModel.js
+++ src/models/blogModel.js
@@ -1,11 +1,12 @@
 import knex from "knex";
 import { db } from "../config/db.js";
 
-const table = "sl_employee_blogs";
+const tableEmployee = "sl_employee_blogs";
+const tableCompany = "sl_company_blogs";
 
 export const Blogs = {
   getAllEmployeeBlogs: async () => {
-    return await db(table)
+    return await db(tableEmployee)
       .leftJoin(
         "sl_employee_blog_images",
         "sl_employee_blogs.eblog_id",
@@ -18,6 +19,20 @@ export const Blogs = {
       .groupBy("sl_employee_blogs.eblog_id");
   },
   addEmployeeBlog: async (blog) => {
-    return await db(table).insert(blog);
+    return await db(tableEmployee).insert(blog);
+  },
+
+  getAllCompanyBlogs: async () => {
+    return await db(tableCompany)
+      .leftJoin(
+        "sl_company_blog_images",
+        "sl_company_blogs.eblog_id",
+        "sl_company_blog_images.eblog_id"
+      )
+      .select(
+        "sl_company_blogs.*",
+        db.raw("JSON_ARRAYAGG(sl_company_blog_images.image_url) AS images")
+      )
+      .groupBy("sl_company_blogs.eblog_id");
   },
 };
