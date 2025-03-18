eb4b4854d0c88da9f5af35cee804eea11fbee696
domingohernani <dev.hernani.domingo@gmail.com>
2025-03-18 18:19:43 +0800

    refactor: update blog model to use constants for table names and improve query readability

--- src/models/blogModel.js
+++ src/models/blogModel.js
@@ -2,22 +2,26 @@ import knex from "knex";
 import { db } from "../config/db.js";
 
 const tableEmployee = "sl_employee_blogs";
+const tableEmployeeImages = "sl_employee_blog_images";
+
 const tableCompany = "sl_company_blogs";
+const tableCompanyImages = "sl_company_blog_images";
 
 export const Blogs = {
   getAllEmployeeBlogs: async () => {
     return await db(tableEmployee)
       .leftJoin(
-        "sl_employee_blog_images",
-        "sl_employee_blogs.eblog_id",
-        "sl_employee_blog_images.eblog_id"
+        tableEmployeeImages,
+        `${tableEmployee}.eblog_id`,
+        `${tableEmployeeImages}.eblog_id`
       )
       .select(
-        "sl_employee_blogs.*",
-        db.raw("JSON_ARRAYAGG(sl_employee_blog_images.image_url) AS images")
+        `${tableEmployee}.*`,
+        db.raw(`JSON_ARRAYAGG(${tableEmployeeImages}.image_url) AS images`)
       )
-      .groupBy("sl_employee_blogs.eblog_id");
+      .groupBy(`${tableEmployee}.eblog_id`);
   },
+
   addEmployeeBlog: async (blog) => {
     return await db(tableEmployee).insert(blog);
   },
@@ -25,14 +29,14 @@ export const Blogs = {
   getAllCompanyBlogs: async () => {
     return await db(tableCompany)
       .leftJoin(
-        "sl_company_blog_images",
-        "sl_company_blogs.eblog_id",
-        "sl_company_blog_images.eblog_id"
+        tableCompanyImages,
+        `${tableCompany}.eblog_id`,
+        `${tableCompanyImages}.eblog_id`
       )
       .select(
-        "sl_company_blogs.*",
-        db.raw("JSON_ARRAYAGG(sl_company_blog_images.image_url) AS images")
+        `${tableCompany}.*`,
+        db.raw(`JSON_ARRAYAGG(${tableCompanyImages}.image_url) AS images`)
       )
-      .groupBy("sl_company_blogs.eblog_id");
+      .groupBy(`${tableCompany}.eblog_id`);
   },
 };
