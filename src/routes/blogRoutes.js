812fa2d39e00d345ce9a91faf8e54db884eb3f7b
domingohernani <dev.hernani.domingo@gmail.com>
2025-03-18 18:11:54 +0800

    refactor: update blog routes to use new controller methods for employee blogs

--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { addBlog, getAllBlogs } from "../controllers/blogController.js";
+import {
+  addEmployeeBlog,
+  getAllEmployeeBlogs,
+} from "../controllers/blogController.js";
 import upload from "../utils/multer.js";
 import verifyJWT from "../middlewares/verifyJWT.js";
 
 const router = express.Router();
 
-router.get("/all-employee-blog", getAllBlogs);
+router.get("/all-employee-blog", getAllEmployeeBlogs);
 
-router.post("/add-employee-blog", verifyJWT, addBlog);
+router.post("/add-employee-blog", verifyJWT, addEmployeeBlog);
 
 export default router;
