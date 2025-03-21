54165612a10ed2cd80c5c1ea8d052b4ee878ba14
ljsalcedo-dev <jau.salcedo04@gmail.com>
2025-03-21 14:54:07 +0800

    feat: implement getCompanyBlogById route and refactor addCompanyBlog method

--- src/models/blogModel.js
+++ src/models/blogModel.js
@@ -104,6 +104,40 @@ export const Blogs = {
       .orderBy("sl_company_blogs.created_at", "desc");
   },
 
+  getCompanyBlogById: async (cblog_id) => {
+    return await db
+      .select(
+        "sl_company_blogs.cblog_id AS cblogId",
+        "title",
+        "description",
+        "created_at AS createdAt",
+        db.raw(
+          "CONCAT(hris_user_infos.first_name, ' ', LEFT(hris_user_infos.middle_name, 1), '. ', hris_user_infos.last_name) AS createdBy"
+        ),
+        db.raw(
+          "IFNULL(JSON_ARRAYAGG(sl_company_blog_images.image_url), JSON_ARRAY()) AS images"
+        )
+      )
+      .from("sl_company_blogs")
+      .innerJoin("hris_user_infos", {
+        "sl_company_blogs.created_by": "hris_user_infos.user_id",
+      })
+      .leftJoin("sl_company_blog_images", {
+        "sl_company_blogs.cblog_id": "sl_company_blog_images.cblog_id",
+      })
+      .where("sl_company_blogs.cblog_id", cblog_id)
+      .groupBy(
+        "sl_company_blogs.cblog_id",
+        "title",
+        "description",
+        "created_at",
+        "hris_user_infos.first_name",
+        "hris_user_infos.middle_name",
+        "hris_user_infos.last_name"
+      )
+      .first();
+  },
+
   addCompanyBlog: async (data) => {
     const cblog_id = uuidv7();
 
