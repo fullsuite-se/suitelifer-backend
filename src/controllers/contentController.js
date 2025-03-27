5a4221eb34e07515c24023394581908085637fbd
ljsalcedo-dev <jau.salcedo04@gmail.com>
2025-03-27 09:35:28 +0800

    feat: enhance insertContent function with additional fields and validation

--- src/controllers/contentController.js
+++ src/controllers/contentController.js
@@ -1,4 +1,5 @@
 import { Content } from "../models/contentModel.js";
+import { now } from "../utils/date.js";
 
 export const getAboutUs = async (req, res) => {
   try {
@@ -12,9 +13,60 @@ export const getAboutUs = async (req, res) => {
 
 export const insertContent = async (req, res) => {
   try {
-    const { homeVideo, textBanner, heroImage, storyImg, } = req.body;
+    const {
+      homeVideo,
+      textBanner,
+      heroImage,
+      storyImage,
+      aboutVideo,
+      missionSlogan,
+      mission,
+      visionSlogan,
+      vision,
+      dayInPodUrl,
+      user_id,
+    } = req.body;
+
+    if (
+      !homeVideo ||
+      !textBanner ||
+      !heroImage ||
+      !storyImage ||
+      !aboutVideo ||
+      !missionSlogan ||
+      !mission ||
+      !visionSlogan ||
+      !vision ||
+      !dayInPodUrl ||
+      !user_id
+    ) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Missing required fields" });
+    }
+
+    const newAboutUs = {
+      home_video: homeVideo,
+      text_banner: textBanner,
+      hero_image: heroImage,
+      story_image: storyImage,
+      about_video: aboutVideo,
+      mission_slogan: missionSlogan,
+      mission,
+      vision_slogan: visionSlogan,
+      vision,
+      day_in_pod_url: dayInPodUrl,
+      created_at: now(),
+      created_by: user_id,
+    };
+
+    await Content.insertAboutUs(newAboutUs);
+
+    res
+      .status(201)
+      .json({ success: true, message: "About Us successfully updated" });
   } catch (err) {
     console.log(err);
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
-}
+};
