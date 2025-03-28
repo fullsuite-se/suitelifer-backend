3e98782a2a0eda4860d955b5eb03e0c4570df055
ljsalcedo-dev <jau.salcedo04@gmail.com>
2025-03-28 16:19:24 +0800

    feat: add insertEvent function and route for creating new events

--- src/models/eventModel.js
+++ src/models/eventModel.js
@@ -1,7 +1,13 @@
 import { db } from "../config/db.js";
 
+const table = () => db("sl_events")
+
 export const Event = {
   getAllEvents: async () => {
-    return await db.select("*").from("sl_events");
+    return await db("sl_events");
   },
+
+  insertEvent: async (newEvent) => {
+    return await table().insert(newEvent);
+  }
 };
