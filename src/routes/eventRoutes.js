1dfe4c9b7f369efd00116bbddf98bc9ddaa66b69
ljsalcedo-dev <jau.salcedo04@gmail.com>
2025-03-28 16:19:24 +0800

    feat: add insertEvent function and route for creating new events

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getAllEvents } from "../controllers/eventController.js";
+import { getAllEvents, insertEvent } from "../controllers/eventController.js";
 
 const router = express.Router();
 
 router.get("/all-events", getAllEvents);
+
+router.post("/add-event", insertEvent);
 
 export default router;
