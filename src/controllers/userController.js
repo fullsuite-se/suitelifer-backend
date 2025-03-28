ceab79afb871f4a5015b993eb186fb1a775ba9cc
domingohernani <dev.hernani.domingo@gmail.com>
2025-03-28 16:54:23 +0800

    feat: add user password update functionality with JWT and bcrypt validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,6 @@
 import { User } from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 export const getUsers = async (req, res) => {
   try {
@@ -6,5 +8,38 @@ export const getUsers = async (req, res) => {
     res.json(users);
   } catch (err) {
     console.log(err);
+  }
+};
+
+export const updateUserPassword = async (req, res) => {
+  const { newPassword, token } = req.body;
+
+  if (!newPassword || !token) {
+    return res
+      .status(400)
+      .json({ isSuccess: false, message: "Invalid request" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = await User.getUser(decoded.userId);
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ isSuccess: false, message: "Invalid request" });
+    }
+
+    const tokenIsValid = await bcrypt.compare(token, user.user_key);
+
+    if (!tokenIsValid) {
+      return res
+        .status(400)
+        .json({ isSuccess: false, message: "Invalid request" });
+    }
+    res.json(user);
+  } catch (error) {
+    console.error(error);
   }
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,10 @@
-import express from 'express';
-import { getUsers } from '../controllers/userController.js';
+import express from "express";
+import { getUsers, updateUserPassword } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/users", getUsers);
+
+router.post("/update-password", updateUserPassword);
 
 export default router;
