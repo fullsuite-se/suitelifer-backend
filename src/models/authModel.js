89072460f8932c9a35a4f048316c18209d5e12ca
domingohernani <dev.hernani.domingo@gmail.com>
2025-04-08 14:58:03 +0800

    feat: implement user registration and email verification functionality

--- src/models/authModel.js
+++ src/models/authModel.js
@@ -3,6 +3,10 @@ import { db } from "../config/db.js";
 const userAccountTable = "hris_user_accounts";
 const userInfos = "hris_user_infos";
 
+// TODO New Tables
+const tempUserAccountTable = "sl_user_accounts";
+const emailVerificationCodeTable = "sl_email_verification_codes";
+
 export const Auth = {
   authenticate: async (email) => {
     return await db.transaction(async (trx) => {
@@ -34,5 +38,25 @@ export const Auth = {
         "service_features.service_feature_id"
       )
       .where("hris_user_access_permissions.user_id", id);
+  },
+
+  getEmailVerificationCodeById: async (id) => {
+    return await db(emailVerificationCodeTable)
+      .where(`${emailVerificationCodeTable}.user_id`, id)
+      .first();
+  },
+
+  registerUser: async (user) => {
+    return await db(tempUserAccountTable).insert(user);
+  },
+
+  addEmailVerificationCode: async (user) => {
+    return await db(emailVerificationCodeTable).insert(user);
+  },
+
+  updateUserVerificationStatus: async (id) => {
+    return await db(tempUserAccountTable).where("user_id", id).update({
+      is_verified: true,
+    });
   },
 };
