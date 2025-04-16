d1dcc98a6b069de3dd2a6cfa1c0eedbbc11ae59e
MB-Tiago <melbraeisantiago@gmail.com>
2025-04-16 14:09:07 +0800

    refactor: streamline blog routes and enhance employee blog functionality

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,16 +1,6 @@
 import { Blogs } from "../models/blogModel.js";
-import { v7 as uuidv7 } from "uuid";
 import { now } from "../utils/date.js";
-
-export const getAllEmployeeBlogs = async (req, res) => {
-  try {
-    const events = await Blogs.getAllEmployeeBlogs();
-    res.status(200).json(events);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-};
+import { v7 as uuidv7 } from "uuid";
 
 export const addEmployeeBlog = async (req, res) => {
   const data = req.body;
@@ -21,6 +11,7 @@ export const addEmployeeBlog = async (req, res) => {
     eblog_id: uuidv7(),
     title: data.title,
     description: data.description,
+    is_shown: data.is_shown ?? true, 
     created_at: now(),
     created_by: userId,
     updated_by: userId,
@@ -29,7 +20,6 @@ export const addEmployeeBlog = async (req, res) => {
   console.dir(blog, { depth: null });
 
   try {
-    // Creating records the eblog table
     await Blogs.addEmployeeBlog(blog);
     res.status(200).json({
       success: true,
@@ -42,71 +32,13 @@ export const addEmployeeBlog = async (req, res) => {
   }
 };
 
-export const getAllCompanyBlogTags = async (req, res) => {
-  try {
-    const blogTags = await Blogs.getAllCompanyBlogTags();
-    res.status(200).json({ success: true, data: blogTags });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-};
 
-export const getAllCompanyBlogs = async (req, res) => {
-  try {
-    const blogs = await Blogs.getAllCompanyBlogs();
-    res.status(200).json(blogs);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-};
-
-export const getFilteredCompanyBlogs = async (req, res) => {
-  try {
-    const { tag_id } = req.params;
-
-    if (!tag_id) {
-      res
-        .status(400)
-        .json({ success: false, message: "Missing required field: tag id" });
+export const getAllEmployeeBlogs = async (req, res) => {
+    try {
+      const blogs = await Blogs.getAllEmployeeBlogs();
+      res.status(200).json(blogs);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: "Internal Server Error" });
     }
-
-    const blogs = await Blogs.getFilteredCompanyBlogs(tag_id);
-
-    res.status(200).json({ success: true, data: blogs });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ success: false, message: "Internal Server Error" });
-  }
-};
-
-export const getCompanyBlogById = async (req, res) => {
-  try {
-    const { cblog_id } = req.params;
-
-    const cblogDetails = await Blogs.getCompanyBlogById(cblog_id);
-
-    res.status(200).json({ success: true, data: cblogDetails });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ success: false, message: "Internal Server Error" });
-  }
-};
-
-export const addCompanyBlog = async (req, res) => {
-  const data = req.body;
-
-  try {
-    const blogId = await Blogs.addCompanyBlog(data);
-
-    res.status(200).json({
-      isSuccess: true,
-      message: "Blog added successfully!",
-      id: blogId,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-};
+  };
--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -1,30 +1,13 @@
 import express from "express";
 import {
-  addCompanyBlog,
   addEmployeeBlog,
-  getAllCompanyBlogs,
-  getAllCompanyBlogTags,
   getAllEmployeeBlogs,
-  getCompanyBlogById,
-  getFilteredCompanyBlogs,
 } from "../controllers/blogController.js";
-
 import verifyJWT from "../middlewares/verifyJWT.js";
 
 const router = express.Router();
 
-router.get("/all-employee-blog", getAllEmployeeBlogs);
-
-router.post("/add-employee-blog", verifyJWT, addEmployeeBlog);
-
-router.get("/all-tags", getAllCompanyBlogTags);
-
-router.get("/all-company-blogs", getAllCompanyBlogs);
-
-router.get("/all-company-blogs/:tag_id", getFilteredCompanyBlogs);
-
-router.get("/get-company-blog/:cblog_id", getCompanyBlogById);
-
-router.post("/add-company-blog", verifyJWT, addCompanyBlog);
+router.get("/employee-allblog", getAllEmployeeBlogs);
+router.post("/employee-blog", verifyJWT, addEmployeeBlog);
 
 export default router;
