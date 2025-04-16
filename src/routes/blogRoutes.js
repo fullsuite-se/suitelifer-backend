122b0bad94af79d279d28a541f3abdcf8feab559
MB-Tiago <melbraeisantiago@gmail.com>
2025-04-16 19:22:55 +0800

    feat: add edit and delete functionality for employee blogs

--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -2,6 +2,8 @@ import express from "express";
 import {
   addEmployeeBlog,
   getAllEmployeeBlogs,
+  deleteEmployeeBlog,
+  editEmployeeBlog,
 } from "../controllers/blogController.js";
 
 import verifyToken from "../middlewares/verifyToken.js";
@@ -11,5 +13,9 @@ const router = express.Router();
 router.get("/all-employee-blog", verifyToken, getAllEmployeeBlogs);
 
 router.post("/add-employee-blog", verifyToken, addEmployeeBlog);
+
+router.post("/edit-employee-blog", editEmployeeBlog);
+
+router.post("/delete-employee-blog", deleteEmployeeBlog);
 
 export default router;
