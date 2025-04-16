9687e843486724c462ef0df9cabcc417f67b48ca
domingohernani <dev.hernani.domingo@gmail.com>
2025-04-16 15:10:38 +0800

    refactor: remove unused company blog routes from blogRoutes.js

--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -12,14 +12,4 @@ router.get("/all-employee-blog", verifyToken, getAllEmployeeBlogs);
 
 router.post("/add-employee-blog", verifyToken, addEmployeeBlog);
 
-router.get("/all-tags", verifyToken, getAllCompanyBlogTags);
-
-router.get("/all-company-blogs", verifyToken, getAllCompanyBlogs);
-
-router.get("/all-company-blogs/:tag_id", verifyToken, getFilteredCompanyBlogs);
-
-router.get("/get-company-blog/:cblog_id", verifyToken, getCompanyBlogById);
-
-router.post("/add-company-blog", verifyToken, addCompanyBlog);
-
 export default router;
