2272f0fdb2c959ed79cb2b5c160b44aaf4200775
domingohernani <dev.hernani.domingo@gmail.com>
2025-04-16 09:24:38 +0800

    feat: add email existence check during registration and implement verification code expiration

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -97,19 +97,29 @@ export const logout = (req, res) => {
 export const register = async (req, res) => {
   const { firstName, middleName, lastName, workEmail, password } = req.body;
 
-  const userId = uuidv7();
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const data = {
-    user_id: userId,
-    user_email: workEmail,
-    user_password: hashedPassword,
-    first_name: firstName,
-    middle_name: middleName,
-    last_name: lastName,
-    created_at: db.fn.now(),
-  };
   try {
+    // Disallow registration if the email is associated with another account
+    const user = await User.getUserByEmail(workEmail);
+    if (user) {
+      return res.status(409).json({
+        isEmailAlreadyExist: true,
+        message: "Email is already registered",
+      });
+    }
+
+    const userId = uuidv7();
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const data = {
+      user_id: userId,
+      user_email: workEmail,
+      user_password: hashedPassword,
+      first_name: firstName,
+      middle_name: middleName,
+      last_name: lastName,
+      created_at: db.fn.now(),
+    };
+
     await Auth.registerUser(data);
     res.status(200).json({
       isSuccess: true,
@@ -139,7 +149,8 @@ export const sendAccountVerificationLink = async (req, res) => {
 
     const publicKeyPEM = process.env.JWE_PUBLIC_KEY;
     const publicKey = await importSPKI(publicKeyPEM, "RSA-OAEP");
-    const payload = JSON.stringify({ code: code, id: userId });
+    const exp = Math.floor(Date.now() / 1000 + 15 * 60);
+    const payload = JSON.stringify({ code: code, id: userId, exp: exp });
     const jwe = await new CompactEncrypt(new TextEncoder().encode(payload))
       .setProtectedHeader({ alg: "RSA-OAEP", enc: "A256GCM" })
       .encrypt(publicKey);
@@ -192,6 +203,14 @@ export const verifyAccountVerificationLink = async (req, res) => {
     const privateKey = await importPKCS8(privateKeyPEM, "RSA-OAEP");
     const { plaintext } = await compactDecrypt(payloadEncrypted, privateKey);
     const payload = JSON.parse(new TextDecoder().decode(plaintext));
+
+    // Checking if yung token is still valid
+    if (Date.now() / 1000 > payload.exp) {
+      return res.status(400).json({
+        isSuccess: false,
+        message: "Verification code has expired. Please request a new one.",
+      });
+    }
 
     const user = await Auth.getVerificationCodeById(payload.id);
 
