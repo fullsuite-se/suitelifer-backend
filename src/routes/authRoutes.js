5970be8dfdc4be8ce62b1d4b4137cd7acea83bc1
domingohernani <dev.hernani.domingo@gmail.com>
2025-04-17 10:10:57 +0800

    fix: integrate reCAPTCHA middleware into login and registration routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,21 +4,21 @@ import {
   logout,
   refreshToken,
   userInfo,
-  verifyApplication,
   sendPasswordResetLink,
   register,
   sendAccountVerificationLink,
   verifyAccountVerificationLink,
 } from "../controllers/authController.js";
 import verifyToken from "../middlewares/verifyToken.js";
+import { recaptcha } from "../middlewares/recaptcha.js";
 
 const router = express.Router();
 
-router.post("/login", login);
+router.post("/login", recaptcha, login);
 
 router.post("/logout", logout);
 
-router.post("/register", register);
+router.post("/register", recaptcha, register);
 
 router.post("/send-account-verification-link", sendAccountVerificationLink);
 
@@ -27,8 +27,6 @@ router.get("/verify-account-verification-link", verifyAccountVerificationLink);
 router.post("/send-password-reset-link", sendPasswordResetLink);
 
 router.get("/refresh-token", refreshToken);
-
-router.post("/verify-recaptcha", verifyApplication);
 
 router.get("/user-info", verifyToken, userInfo);
 
