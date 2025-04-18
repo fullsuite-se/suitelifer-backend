35ac0fbd29b6827b5af719aa2d766c07c0b1900b
ljsalcedo-dev <jau.salcedo04@gmail.com>
2025-04-18 15:21:28 +0800

    feat: add CRUD operations for newsletter issues and enhance newsletter management

--- src/routes/newsletterRoutes.js
+++ src/routes/newsletterRoutes.js
@@ -1,13 +1,25 @@
 import express from "express";
 import {
+  deleteNewsletter,
+  getIssues,
   getNewsletters,
+  insertIssue,
   insertNewsletter,
+  updateNewsletter,
 } from "../controllers/newsletterController.js";
 
 const router = express.Router();
 
+router.get("/issues", getIssues);
+
+router.post("/issues", insertIssue);
+
 router.get("/newsletter", getNewsletters);
 
 router.post("/newsletter", insertNewsletter);
+
+router.put("/newsletter", updateNewsletter);
+
+router.delete("/newsletter", deleteNewsletter);
 
 export default router;
