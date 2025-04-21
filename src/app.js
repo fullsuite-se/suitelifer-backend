8d00613c0087e0be3007452b0bdff8210e433e29
domingohernani <dev.hernani.domingo@gmail.com>
2025-04-21 16:58:15 +0800

    fix: comment out static file serving for public directory and log path for debugging

--- src/app.js
+++ src/app.js
@@ -33,7 +33,10 @@ app.use(helmet());
 
 const srcFolder = path.dirname(new URL(import.meta.url).pathname);
 const publicFolder = path.join(srcFolder, "..", "public");
-app.use(express.static(publicFolder));
+
+console.log(publicFolder);
+
+// app.use(express.static(publicFolder));
 
 app.use(
   cors({
