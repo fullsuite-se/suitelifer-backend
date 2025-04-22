b0ea9c2820dd4a6be7531b9a13264c14a1218c82
ljsalcedo-dev <jau.salcedo04@gmail.com>
2025-04-22 12:09:30 +0800

    feat: add audit log routes to the main application

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ import faqsRoutes from "./routes/faqsRoutes.js";
 import contactRoutes from "./routes/contactRoutes.js";
 import newsletterRoutes from "./routes/newsletterRoutes.js";
 import certificationRoutes from "./routes/certificationRoutes.js";
+import auditLogRoutes from "./routes/auditLogRoutes.js";
 
 const app = express();
 dotenv.config();
@@ -54,5 +55,6 @@ app.use("/api", faqsRoutes);
 app.use("/api", contactRoutes);
 app.use("/api", newsletterRoutes);
 app.use("/api", certificationRoutes);
+app.use("/api", auditLogRoutes);
 
 export default app;
