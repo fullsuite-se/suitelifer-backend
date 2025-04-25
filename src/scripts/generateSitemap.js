a380b1d8d3c2a5007fb1573f0a3ee7836b94af71
domingohernani <dev.hernani.domingo@gmail.com>
2025-04-25 11:52:16 +0800

    refactor: modularize sitemap generation by extracting career and newsletter URL functions

--- src/scripts/generateSitemap.js
+++ src/scripts/generateSitemap.js
@@ -1,54 +1,8 @@
-import axios from "axios";
 import fs from "fs/promises";
 import path from "path";
 import { fileURLToPath } from "url";
-import { toSlug } from "../utils/slugUrl.js";
-
-const generateCareerUrls = async () => {
-  const today = new Date().toISOString().split("T")[0];
-
-  let urls = [
-    `<url>
-<loc>https://www.suitelifer.com/careers</loc>
-<lastmod>${today}</lastmod>
-<changefreq>monthly</changefreq>
-<priority>0.8</priority>
-</url>`,
-  ];
-
-  const response = await axios.get(
-    `${process.env.ATS_API_BASE_URL}/jobs/shown`
-  );
-  const jobs = response.data.data;
-
-  jobs.forEach((job) => {
-    const url = `<url>
-<loc>https://www.suitelifer.com/careers/${toSlug(job.jobTitle)}?id=${
-      job.jobId
-    }</loc>
-<lastmod>${today}</lastmod>
-<changefreq>monthly</changefreq>
-<priority>0.8</priority>
-</url>`;
-    urls.push(url);
-  });
-  return urls.join("");
-};
-
-const generateNewsletterUrls = async () => {
-  const today = new Date().toISOString().split("T")[0];
-
-  let urls = [
-    `<url>
-<loc>https://www.suitelifer.com/newsletter</loc>
-<lastmod>${today}</lastmod>
-<changefreq>weekly</changefreq>
-<priority>0.8</priority>
-</url>`,
-  ];
-
-  return urls.join("");
-};
+import generateCareerUrls from "./generateCareerUrls.js";
+import generateNewsletterUrls from "./generateNewsletterUrls.js";
 
 const generateSitemap = async () => {
   const currentPath = fileURLToPath(import.meta.url);
@@ -60,10 +14,10 @@ const generateSitemap = async () => {
     "public",
     "sitemap.xml"
   );
+
   try {
     const careers = await generateCareerUrls();
     const newsletters = await generateNewsletterUrls();
-    // console.log(newsletters);
 
     const newSitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
@@ -92,6 +46,7 @@ const generateSitemap = async () => {
         <priority>0.8</priority>
       </url>
       ${careers}
+      ${newsletters}
     </urlset>`;
 
     try {
