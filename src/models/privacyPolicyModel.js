53bd156fee8eb021d6ea06b34235a1e716c688ec
MB-Tiago <melbraeisantiago@gmail.com>
2025-04-25 11:05:26 +0800

    feat: implement CRUD operations for privacy policies with routing and error handling

---
+++ src/models/privacyPolicyModel.js
@@ -0,0 +1,34 @@
+import { db } from "../config/db.js";
+
+const table = () => db("sl_privacy_policy");
+
+export const PrivacyPolicy = {
+  getAllPolicies: async () => {
+    return await db
+      .select(
+        "policy_id AS policyId",
+        "title",
+        "description",
+        "sl_privacy_policy.created_at AS createdAt",
+        db.raw(
+          "CONCAT(first_name, ' ', LEFT(middle_name, 1), '. ', last_name) AS createdBy"
+        )
+      )
+      .from("sl_privacy_policy")
+      .innerJoin("sl_user_accounts", {
+        "sl_privacy_policy.created_by": "sl_user_accounts.user_id",
+      });
+  },
+
+  addPolicy: async (data) => {
+    return await table().insert(data);
+  },
+
+  updatePolicy: async (id, data) => {
+    return await table().where({ policy_id: id }).update(data);
+  },
+
+  deletePolicy: async (id) => {
+    return await table().where({ policy_id: id }).del();
+  },
+};
