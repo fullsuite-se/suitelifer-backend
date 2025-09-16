36dbb4fe9e9605c314795285fddb0d8b2064ca51
allenjalvaro <allenjalvaro@gmail.com>
2025-09-17 07:27:28 +0800

    Remove commented-out knex configuration code for clarity

--- src/config/knexConfig.js
+++ src/config/knexConfig.js
@@ -1,36 +1,3 @@
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// const PORT = process.env.PORT;
-// const DB_HOST = process.env.DB_HOST;
-// const DB_PORT = process.env.DB_PORT;
-// const DB_USER = process.env.DB_USER;
-// const DB_PASSWORD = process.env.DB_PASSWORD;
-// const DB_DATABASE = process.env.DB_DATABASE;
-
-// export const knexconfig = {
-//   client: "mysql2",
-//   connection: {
-//     host: DB_HOST,
-//     port: DB_PORT,
-//     user: DB_USER,
-//     password: DB_PASSWORD,
-//     database: DB_DATABASE,
-//     connectTimeout: 60000,
-//   },
-//   pool: { 
-//     min: 0, 
-//     max: 5,
-//     acquireTimeoutMillis: 60000,
-//     createTimeoutMillis: 30000,
-//     destroyTimeoutMillis: 5000,
-//     idleTimeoutMillis: 30000,
-//     reapIntervalMillis: 1000,
-//     createRetryIntervalMillis: 200
-//   },
-// };
-
-
 import dotenv from 'dotenv';
 dotenv.config();
 
