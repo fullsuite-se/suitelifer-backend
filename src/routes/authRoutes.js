a38b1351f0f4375f13c429f9759e624228f8c199
allenjalvaro <allenjalvaro@gmail.com>
2025-10-22 10:51:57 +0800

    fix: returned verifyToken back

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -34,6 +34,6 @@ router.get("/refresh-token", refreshToken);
 
 router.post("/verify-recaptcha", verifyApplication);
 
-router.get("/user-info", userInfo);// Temporarily removed verifyToken for testing
+router.get("/user-info", verifyToken, userInfo);
 
 export default router;
