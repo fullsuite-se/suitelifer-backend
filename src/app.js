2485b2c9f04a4f6e65b5064f1313d158ff1390a2
allenjalvaro <allenjalvaro@gmail.com>
2025-10-23 13:44:40 +0800

    fix: fixing cors

--- src/app.js
+++ src/app.js
@@ -40,6 +40,7 @@ const allowedOrigins = [
   process.env.LIVE_URL,
   process.env.HRIS_VITE_API_BASE_URL,
   process.env.HRIS_VITE_FRONTEND_BASE_URL,
+  "https://kriya-hris.vercel.app",
   "https://kriya-hris-backend.vercel.app",
   "http://localhost:5173",
   "http://localhost:5174",
